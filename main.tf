e5557cf7fd9a796e316262c3a00f8268fe834b8c
nazikmusa <musaevan331@gmail.com>
2025-01-25 02:20:09 +0000

    igw added

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnet_cidr
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
